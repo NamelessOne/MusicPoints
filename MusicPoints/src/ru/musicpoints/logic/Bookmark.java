e45cf1125aeb2481532eaf897eb78ed10491d32b
Namelessone <nlessone@gmail.com>
2012-07-23 18:53:29 +0400

    Первый осмысленный коммит=) Добавлены классы для взаимодействия Hibernate с базой. Пока без аннотаций и маппингов.

--- MusicPoints/src/ru/musicpoints/logic/Bookmark.java
+++ MusicPoints/src/ru/musicpoints/logic/Bookmark.java
@@ -1,6 +1,31 @@
 package ru.musicpoints.logic;
 
 public class Bookmark {
-	int ID;
-	String Name;
+	private int ID;
+	private int user_id;
+	private int music_point_id;
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public int getMusic_point_id() {
+		return music_point_id;
+	}
+
+	public void setMusic_point_id(int music_point_id) {
+		this.music_point_id = music_point_id;
+	}
 }
