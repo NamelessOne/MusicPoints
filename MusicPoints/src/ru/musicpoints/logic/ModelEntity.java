d744f249f074175824435c07a5351e9b8211115e
mpanteleev <skaute@yandex.ru>
2012-07-31 02:28:48 +0400

    all classes in *.logic package extends from ModelEntity class

---
+++ MusicPoints/src/ru/musicpoints/logic/ModelEntity.java
@@ -0,0 +1,13 @@
+package ru.musicpoints.logic;
+
+public class ModelEntity {
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
